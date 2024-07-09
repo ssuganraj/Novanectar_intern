e80c3acdd7307a736d2f00f87bc72ffe3b8373cb
ssuganraj <suganrajsms@gmail.com>
2024-07-09 10:16:21 +0530

    bangok.jpg file added

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('signin-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign In form submitted!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Contact form submitted!');
+});
